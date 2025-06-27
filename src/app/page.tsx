9b925dd74a5139e6caeb31ce74f6d085b707a992
efal-yw <yuki43wakita@icloud.com>
2025-06-27 20:51:26 +0900

    Hola Mund

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">
-        Hello, World! This is the top page.
+        Hola, Mund! This is the top page.
       </h1>
       <p className="mt-4">
         If you can see this, the deployment is working correctly.
