4a4308e1f077faa7e4bac3da4c359eb0bc9bbfed
efal-yw <yuki43wakita@icloud.com>
2025-09-14 02:17:25 +0900

    Update page.tsx　内容を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">
-        Hola, Mund! This is the top page.
+        グッドモーニング、わたしのブログへ
       </h1>
       <p className="mt-4">
-        If you can see this, the deployment is working correctly.
+        これは脇田悠希のブログです。
       </p>
     </main>
   )
